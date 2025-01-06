3f7988b8194e4ad0eb70db6261f687b0984a1ce1
pxsswordprotected <jamesalford661@gmail.com>
2025-01-06 11:53:17 -0600

    second init

---
+++ scheduler.js
@@ -0,0 +1,8 @@
+const schedule = require('node-schedule');
+const Push = require('node-pushover');
+
+const pushover = new Push ({
+    token: 'ag7zoznf8yugfc4fhetgczwgei4cvq',
+    user: 'uciypfwyvr83bytp1u3eknmebv8a4b'
+});
+
